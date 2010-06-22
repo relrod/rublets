4fc61a02634684bf330f1bcb23900716b55fe2b1
Nick Markwell <nick@duckinator.net>
2010-06-22 01:36:19 -0400

    Fixed tabbing fails

--- rubino.rb
+++ rubino.rb
@@ -8,9 +8,9 @@ load File.join(File.dirname(__FILE__), 'lib', 'bot.rb')
 
 
 if ARGV.size > 0
-	manager = Rubino::Manager.new(ARGV[0])
+  manager = Rubino::Manager.new(ARGV[0])
 else
-	manager = Rubino::Manager.new(File.join(File.dirname(__FILE__), 'config.yaml'))
+  manager = Rubino::Manager.new(File.join(File.dirname(__FILE__), 'config.yaml'))
 end
 
 manager.run
