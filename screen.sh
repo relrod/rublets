0d851daaec7e87da567ee6b67c24bac071a8fa7e
Nick Markwell <nick@duckinator.net>
2011-10-30 03:13:25 -0400

    screen.sh should actually work now

--- screen.sh
+++ screen.sh
@@ -4,8 +4,13 @@ DIR=$(dirname $(readlink -f $0))
 
 CONF="$DIR/$1"
 
-if [ "$CONF" == "" ]; then
+if [ ! -f "$CONF" ]; then
   CONF="$DIR/config.yaml"
+fi
+
+if [ -f "$CONF" ]; then
+  echo "Config does not exist."
+  exit 1
 fi
 
 screen -dmS rubino $DIR/rubino.rb $CONF
