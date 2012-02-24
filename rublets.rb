6a8c88b4d8d72a7cbfdac788174db22d18c4f99a
Ricky Elrod <ricky@elrod.me>
2012-02-24 04:11:02 -0500

    php eval

--- rublets.rb
+++ rublets.rb
@@ -200,6 +200,22 @@ end
         sandbox.rm_home!
       end
 
+    when /^!php> (.*)/
+      future do
+        sandbox = Sandbox.new(
+          :path => File.expand_path('~/.rublets'),
+          :evaluate_with => ['php'],
+          :timeout => 5,
+          :extension => 'php',
+          :owner => sender.nick,
+          :output_limit_before_gisting => 2,
+          :code => $1
+        )
+        result = sandbox.evaluate
+        result.each { |line| respond line }
+        sandbox.rm_home!
+      end
+
       # Ruby eval.
     when /^!([\w\.\-]+)?>> (.*)/
       # Pull these out of the regex here, because the global captures get reset below.
