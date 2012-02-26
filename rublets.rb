79497a7ea9542048b012bf0a28bf0eabc029b7fa
Ricky Elrod <ricky@elrod.me>
2012-02-26 00:31:44 -0500

    Take out some debug code that I left in.

--- rublets.rb
+++ rublets.rb
@@ -78,6 +78,22 @@ end
         sandbox.rm_home!
       end
 
+    when /^!java> (.*)/
+      future do
+        sandbox = Sandbox.new(
+          :path          => File.expand_path('~/.rublets'),
+          :evaluate_with => ['bash', 'run-java.sh']
+          :timeout       => 20,
+          :extension     => 'java',
+          :owner         => sender.nick,
+          :output_limit  => 2,
+          :code          => $1
+          )
+        result = sandbox.evaluate
+        result.each { |line| respond line }
+        sandbox.rm_home!
+      end
+
     when /^!c> (.*)/
       includes = [
         '#include <stdio.h>',
@@ -206,7 +222,6 @@ end
       else
         code = "<?php #{$2}"
       end
-      puts code
       future do
         sandbox = Sandbox.new(
           :path          => File.expand_path('~/.rublets'),
