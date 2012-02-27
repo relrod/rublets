1809ee25c30162e91048cc7577f8e1c348c62084
Ricky Elrod <ricky@elrod.me>
2012-02-26 19:23:37 -0500

    Perl. Why not.

--- rublets.rb
+++ rublets.rb
@@ -184,6 +184,22 @@ end
         sandbox.rm_home!
       end
 
+    when /^!perl> (.*)/
+      future do
+        sandbox = Sandbox.new(
+          :path          => File.expand_path('~/.rublets'),
+          :evaluate_with => ['perl'],
+          :timeout       => 5,
+          :extension     => 'pl',
+          :owner         => sender.nick,
+          :output_limit  => 2,
+          :code          => $1
+        )
+        result = sandbox.evaluate
+        result.each { |line| respond line }
+        sandbox.rm_home!
+      end
+
     when /^!io> (.*)/
       future do
         sandbox = Sandbox.new(
