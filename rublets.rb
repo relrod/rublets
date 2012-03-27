073f75840fc25e8d32c875b83af396efa12e8827
Ricky Elrod <ricky@elrod.me>
2012-03-27 01:00:54 -0400

    Make ruby be futurized too.

--- rublets.rb
+++ rublets.rb
@@ -513,65 +513,66 @@ end
 
       # Ruby eval.
     when /^!([\w\.\-]+)?>> (.*)/
-      # Pull these out of the regex here, because the global captures get reset below.
-      given_version = $1 # might be nil.
-      code = $2
+      future do
+        # Pull these out of the regex here, because the global captures get reset below.
+        given_version = $1 # might be nil.
+        code = $2
 
-      #future do # We can have multiple evaluations going on at once.
-      rubyversion = 'ruby-1.9.3-p0' # Default, set here for scoping. TODO: Config-file this.
+        #future do # We can have multiple evaluations going on at once.
+        rubyversion = 'ruby-1.9.3-p0' # Default, set here for scoping. TODO: Config-file this.
 
-      # If a version is given (so not default), scan ./rubies/* to see if it matches.
-      # If there is one (and only one) match, move along and set rubyversion to that.
-      # If there's more than one, or no match, warn the user and ignore the eval.
-      unless given_version.nil?
-        if given_version == 'all'
-          rubyversion = 'all'
-        else
-          rubies = Dir['./rubies/*'].map { |a| File.basename(a) }
-          rubies = rubies.delete_if { |ruby| ruby.scan(given_version).empty? }
-          if rubies.count > 1
-            respond "#{sender.nick}: You matched multiple rubies. Be more specific. See !rubies for the full list." and next
-          elsif rubies.count == 0
-            respond "#{sender.nick}: That ruby isn't available. See !rubies for a list." and next
+        # If a version is given (so not default), scan ./rubies/* to see if it matches.
+        # If there is one (and only one) match, move along and set rubyversion to that.
+        # If there's more than one, or no match, warn the user and ignore the eval.
+        unless given_version.nil?
+          if given_version == 'all'
+            rubyversion = 'all'
+          else
+            rubies = Dir['./rubies/*'].map { |a| File.basename(a) }
+            rubies = rubies.delete_if { |ruby| ruby.scan(given_version).empty? }
+            if rubies.count > 1
+              respond "#{sender.nick}: You matched multiple rubies. Be more specific. See !rubies for the full list." and next
+            elsif rubies.count == 0
+              respond "#{sender.nick}: That ruby isn't available. See !rubies for a list." and next
+            end
+            rubyversion = rubies[0]
           end
-          rubyversion = rubies[0]
         end
+
+        eval_code = "result = ::Kernel.eval(#{code.inspect}, TOPLEVEL_BINDING)"
+        if rubyversion == 'all'
+          eval_code += "\n" + 'puts RUBY_VERSION + " #{\'(\' + RUBY_ENGINE + \')\' unless defined?(RUBY_ENGINE).nil?} => " + result.inspect'
+        else
+          eval_code += "\n" + 'puts "=> " + result.inspect'
+        end
+
+        sandbox = Sandbox.new(
+          :path                => File.expand_path('~/.rublets'),
+          :evaluate_with       => ['bash', 'run-ruby.sh', rubyversion],
+          :timeout             => 5,
+          :extension           => 'rb',
+          :owner               => sender.nick,
+          :output_limit        => 2,
+          :code                => eval_code,
+          :binaries_must_exist => ['ruby', 'bash'],
+          )
+
+        sandbox.copy 'rvm', '.rvm'
+
+        # If the user wants to eval against all rubies, then copy the entire rubies directory.
+        if rubyversion == 'all'
+          sandbox.copy 'rubies', '.rvm/rubies/'
+        else
+          sandbox.mkdir(".rvm/rubies")
+          sandbox.copy "rubies/#{rubyversion}", ".rvm/rubies/#{rubyversion}"
+        end
+
+        # This is a bit of a hack, but lets us set up the rvm environment and call the script.
+        sandbox.copy 'eval/run-ruby.sh', 'run-ruby.sh'
+        result = sandbox.evaluate
+        result.each { |line| respond line }
+        sandbox.rm_home!
       end
-
-      eval_code = "result = ::Kernel.eval(#{code.inspect}, TOPLEVEL_BINDING)"
-      if rubyversion == 'all'
-        eval_code += "\n" + 'puts RUBY_VERSION + " #{\'(\' + RUBY_ENGINE + \')\' unless defined?(RUBY_ENGINE).nil?} => " + result.inspect'
-      else
-        eval_code += "\n" + 'puts "=> " + result.inspect'
-      end
-
-      sandbox = Sandbox.new(
-        :path                => File.expand_path('~/.rublets'),
-        :evaluate_with       => ['bash', 'run-ruby.sh', rubyversion],
-        :timeout             => 5,
-        :extension           => 'rb',
-        :owner               => sender.nick,
-        :output_limit        => 2,
-        :code                => eval_code,
-        :binaries_must_exist => ['ruby', 'bash'],
-        )
-
-      sandbox.copy 'rvm', '.rvm'
-
-      # If the user wants to eval against all rubies, then copy the entire rubies directory.
-      if rubyversion == 'all'
-        sandbox.copy 'rubies', '.rvm/rubies/'
-      else
-        sandbox.mkdir(".rvm/rubies")
-        sandbox.copy "rubies/#{rubyversion}", ".rvm/rubies/#{rubyversion}"
-      end
-
-      # This is a bit of a hack, but lets us set up the rvm environment and call the script.
-      sandbox.copy 'eval/run-ruby.sh', 'run-ruby.sh'
-      result = sandbox.evaluate
-      result.each { |line| respond line }
-      sandbox.rm_home!
-      #    end
     end # end case
   rescue ThreadError
     respond "Could not create thread."
