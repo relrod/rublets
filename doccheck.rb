c7623debc803a20eef8087b504b01e1df8c842fe
Ricky Elrod <ricky@elrod.me>
2012-08-07 06:10:25 -0400

    fix up doccheck a bit, it is still broken though. will finish tomorrow.

--- doccheck.rb
+++ doccheck.rb
@@ -12,16 +12,20 @@ def sanitize_language(lang_string)
   lang_string.downcase.gsub(' ', '').gsub(/\(.*/, '')
 end
 
-# Construct a hash of {'lang_name' => ['all', 'aliases']}
-mapped_languages = {}
-Language.languages.keys.each do |language|
-  mapped_languages[language] = (Language.by_name(language)[:aliases].to_a + [language]).collect { |l| sanitize_language(l) }
-end
-
 readme = Nokogiri::HTML(File.open(File.dirname(__FILE__) + '/README.md'))
 documented_languages = readme.css('table a').collect { |link| sanitize_language(link.content) }
 
-undocumented = documented_languages - (mapped_languages.values.flatten & documented_languages)
+undocumented = []
+Language.languages.each do |language, values|
+  needed = true
+  next unless (documented_languages & ([language] + [values[:aliases]]).compact).empty?
+  ([language] + [values[:aliases]]).compact.each do |l|
+   # next if documented_languages.include? l
+    next unless needed
+    undocumented << sanitize_language(language)
+    needed = false
+  end
+end
 
 if undocumented.empty?
   puts "[PASS] All languages are documented!"
