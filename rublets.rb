9858075f2c55e3cbaead18c6659dac6aa0cdc680
Ricky Elrod <ricky@elrod.me>
2013-05-17 13:30:13 -0400

    it burns

--- rublets.rb
+++ rublets.rb
@@ -133,7 +133,7 @@ end
       respond "#{1.chr}ACTION supports: #{Language.list_all}#{1.chr}"
 
     when /^#{Configru.comchar}#{Configru.comchar}lang(?:s|uages)$/
-      respond "Eval.so supports: #{Evalso.languages.values.map(&:name).join(', ')}"
+      respond "Eval.so supports: #{Evalso.languages.values.map(&:name).sort.join(', ')}"
 
     # Ruby eval.
     when /^#{Configru.comchar}(([\w\.\-]+)?>?|>)> (.*)/
