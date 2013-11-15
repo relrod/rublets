a7b21e58d91cb660c687afd70047a3c4b242e52f
Ricky Elrod <ricky@elrod.me>
2013-11-14 21:20:24 -0500

    sort includes list

--- eval/rublets-cpp.h
+++ eval/rublets-cpp.h
@@ -1,18 +1,19 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdint>
-#include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
 #include <deque>
-#include <sstream>
 #include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <thread>
-#include <mutex>
 #include <functional>
-#include <numeric>
+#include <iomanip>
+#include <iostream>
 #include <iterator>
+#include <map>
+#include <mutex>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+
 using namespace std;
 
