00fa37d51c248f73b62e73fa5dc07e59373578b9
Ricky Elrod <ricky@elrod.me>
2014-03-14 20:38:58 -0400

    apparently not like that

--- rublets.rb
+++ rublets.rb
@@ -64,7 +64,7 @@ end
 @bot.on :privmsg do
   puts "#{Time.now} #{params[0]} <#{sender.nick}> #{params[1]}"
   if params[0] == Configru.nickname &&
-      !Configru.servers[server.name.to_s].pm_hosts.include? sender.host
+      !Configru.servers[server.name.to_s].pm_hosts.include?(sender.host)
     next
   end
   limit = Configru.servers[server.name.to_s].channel_options
